d48aba5ed7f9c820f31bdc5b4b9b39587895f7a8
Soham Banik <soham.banik14@gmail.com>
2018-09-07 11:46:37 +0530

    End of Sec 7

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -10,6 +10,7 @@ import NavBar from "../../features/nav/NavBar/NavBar";
 import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
 import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import HomePage from "../../features/home/HomePage";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
