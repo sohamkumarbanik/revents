b5b84fd0acdf247fd5ccabe75c09fc2fbfaa83ad
Soham Banik <soham.banik14@gmail.com>
2018-09-18 12:16:29 +0530

    End of Section 13

--- src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
+import format from "date-fns/format";
 
 const eventImageStyle = {
-    filter: 'brightness(30%)'
+  filter: "brightness(30%)"
 };
 
 const eventImageTextStyle = {
-    position: 'absolute',
-    bottom: '5%',
-    left: '5%',
-    width: '100%',
-    height: 'auto',
-    color: 'white'
+  position: "absolute",
+  bottom: "5%",
+  left: "5%",
+  width: "100%",
+  height: "auto",
+  color: "white"
 };
 
-const EventDetailedHeader = ({event}) => {
+const EventDetailedHeader = ({ event }) => {
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
-        <Image src={`/assets/categoryImages/${event.category}.jpg`} fluid style={eventImageStyle} />
+        <Image
+          src={`/assets/categoryImages/${event.category}.jpg`}
+          fluid
+          style={eventImageStyle}
+        />
 
         <Segment basic style={eventImageTextStyle}>
           <Item.Group>
@@ -31,7 +36,9 @@ const EventDetailedHeader = ({event}) => {
                   content={event.title}
                   style={{ color: "white" }}
                 />
-                <p>{event.date}</p>
+                <p>
+                  {format(event.date, "dddd Do MMMM")}
+                </p>
                 <p>
                   Hosted by <strong>{event.hostedBy}</strong>
                 </p>
@@ -45,7 +52,12 @@ const EventDetailedHeader = ({event}) => {
         <Button>Cancel My Place</Button>
         <Button color="teal">JOIN THIS EVENT</Button>
 
-        <Button as={Link} to={`/manage/${event.id}`} color="orange" floated="right">
+        <Button
+          as={Link}
+          to={`/manage/${event.id}`}
+          color="orange"
+          floated="right"
+        >
           Manage Event
         </Button>
       </Segment>
--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Grid, Icon, Segment, Button } from "semantic-ui-react";
+import format from "date-fns/format";
 
 import EventDetailedMap from "./EventDetailedMap";
 
@@ -7,6 +8,12 @@ class EventDetailedInfo extends Component {
   state = {
     showMap: false
   };
+
+  componentWillUnmount() {
+    this.setState({
+      showMap: false
+    })
+  }
 
   showMapToggle = () => {
     this.setState(prevState => ({
@@ -34,7 +41,10 @@ class EventDetailedInfo extends Component {
               <Icon name="calendar" size="large" color="teal" />
             </Grid.Column>
             <Grid.Column width={15}>
-              <span>{event.date}</span>
+              <span>
+                {format(event.date, "dddd Do MMMM")} at{" "}
+                {format(event.date, "h:mm A")}
+              </span>
             </Grid.Column>
           </Grid>
         </Segment>
@@ -51,7 +61,7 @@ class EventDetailedInfo extends Component {
                 onClick={this.showMapToggle}
                 color="teal"
                 size="tiny"
-                content={this.state.showMap ? 'Hide Map' : 'Show Map'}
+                content={this.state.showMap ? "Hide Map" : "Show Map"}
               />
             </Grid.Column>
           </Grid>
